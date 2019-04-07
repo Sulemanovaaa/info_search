9931f8288a1c52af15efe043eb80e21c20860cf8
OrtyomZ <zltkhnrtm@gmail.com>
2019-04-07 21:26:58 +0300

    links

--- crawler.py
+++ crawler.py
@@ -1,16 +1,28 @@
+import pickle
+from typing import Dict, List
+
 import requests
-import lxml
-from lxml import html
 from bs4 import BeautifulSoup
+from lxml import html
 
 handled_url = []
+link_map = dict()
+redirect_map = dict()
 
-def web(page, WebUrl, baseUrl, xpathList):
-    if(page > 0):
+
+def web(page, WebUrl, baseUrl, xpathList, from_page=None):
+    if (page > 0):
+        name = str(page) + '.txt'
+        if from_page is not None:
+            l = redirect_map.get(from_page, [])
+            if len(l) == 0:
+                redirect_map[from_page] = l
+            l.append(name)
+        link_map[name] = WebUrl
         code = requests.get(WebUrl)
         s = BeautifulSoup(code.text, "html.parser")
         tree = html.fromstring(code.content)
-        path=str('./pages/%d.txt'%(page))
+        path = str('./pages/' + name)
         f = open(path, 'w')
         f.write(s.get_text())
         f.close()
@@ -21,10 +33,33 @@ def web(page, WebUrl, baseUrl, xpathList):
         for link in links:
             if (page > 0):
                 if (isLinkValid(link)):
-#                    print(href)
+                    #                    print(href)
                     handled_url.append(link)
-                    page -= 1  
-                    web(page, str(baseUrl + link), baseUrl, xpathList)          
+                    page -= 1
+                    web(page, str(baseUrl + link), baseUrl, xpathList, name)
+
+
+def save_link_map(lin: Dict[str, str]):
+    with open('./links.pickle', 'wb') as f:
+        pickle.dump(lin, f)
+
+
+def load_link_map() -> Dict[str, str]:
+    with open('./links.pickle', 'rb') as f:
+        lin = pickle.load(f)
+    return lin
+
+
+def save_redirect_map(links: Dict[str, List[str]]):
+    with open('./redirect.pickle', 'wb') as f:
+        pickle.dump(links, f)
+
+
+def load_redirect_map() -> Dict[str, List[str]]:
+    with open('./redirect.pickle', 'rb') as f:
+        red = pickle.load(f)
+    return red
+
 
 def isLinkValid(href):
     if (href == None):
@@ -38,8 +73,11 @@ def isLinkValid(href):
     return True
 
 
-web(100,'https://www.opennet.ru/opennews', 'https://www.opennet.ru',
+if __name__ == "__main__":
+    web(100, 'https://www.opennet.ru/opennews', 'https://www.opennet.ru',
         [
-            '//table[@class="ttxt"]//td[@class="titlebr"]/a[@class="title2"]/@href',
-            '//table[@class="ttxt"]//tr/td[@colspan="3"]/a/@href',
+            '//table[@class="ttxt2"]//td[@class="titlebr"]/a[@class="title2"]/@href',
+            '//table[@class="ttxt2"]//tr/td[@colspan="3"]/a/@href',
         ])
+    save_link_map(link_map)
+    save_redirect_map(redirect_map)
